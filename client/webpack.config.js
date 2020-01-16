5f79427aed3309df718b9abcddcf928829899cbe
Ethan <ebonsignori@gmail.com>
2020-01-15 19:57:53 -0500

    Add webpack, babel

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+        resolve: {
+          extensions: ['.js', '.jsx']
+        },
+      }
+    ]
+  },
+  devServer: {
+    hot: true
+  }
+};
